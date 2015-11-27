cfa30dd3bcad7260c8ef7a5f7dd30efe22f69f5b
Ahmet Özışık <ozisikahmet@gmail.com>
2015-11-27 21:41:59 +0200

    Update Client.php

--- src/j42/LaravelFirebase/Client.php
+++ src/j42/LaravelFirebase/Client.php
@@ -1,1 +1,30 @@
 
+<?php namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use J42\LaravelFirebase\Client;
+use Event;
+
+class LumenFirebaseServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     * @return void
+     */
+    public function register()
+    {
+
+        // Get Connection
+        $config = [
+            'host' => env('FIREBASE_URL'),
+            'token' => env('FIREBASE_SECRET'),
+            'timeout' => 3
+        ];
+
+        // Root provider
+        $this->app->singleton('firebase', function ($app) use ($config) {
+            return new Client($config);
+        });
+    }
+}
