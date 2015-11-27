b91d9a25b2e306d985f88cf8eda3330aa9dc4266
Ahmet Özışık <ozisikahmet@gmail.com>
2015-11-27 21:42:53 +0200

    Create LumenServiceProvider.php

---
+++ src/j42/LaravelFirebase/LumenServiceProvider.php
@@ -0,0 +1,29 @@
+<?php namespace J42\LaravelFirebase;
+
+use Illuminate\Support\ServiceProvider;
+use J42\LaravelFirebase\Client;
+use Event;
+
+class LumenFirebaseServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     * @return void
+     */
+    public function register()
+    {
+
+        // Get Connection
+        $config = [
+            'host' => env('FIREBASE_URL'),
+            'token' => env('FIREBASE_SECRET'),
+            'timeout' => 3
+        ];
+
+        // Root provider
+        $this->app->singleton('firebase', function ($app) use ($config) {
+            return new Client($config);
+        });
+    }
+}
